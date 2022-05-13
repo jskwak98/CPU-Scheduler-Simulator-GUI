6b8f39ac47191509ef29d0a4f942d9bdb6f49a4a
jskwak98 <jskwak98@naver.com>
2022-05-14 01:19:16 +0900

    Added reference directory, share ideas and experimental codes by this folder

--- reference/FCFS_example.py
+++ reference/FCFS_example.py
@@ -1,6 +1,16 @@
 """
 FCFS Scheduler
-example
+example pseudo code
+
+hope it helps how Process class works and Scheduler do its work
+
+note that what scheduler do is only
+1. decide which process should use CPU in current time
+2. keep track of current time
+3. then add the information (pid, last_time_out) to schedule
+
+you do not need to worry about WT, RT, TT
+it's all computed within Process class only if you use_cpu(burst_time, current_time)
 """
 from scheduler import Scheduler
 from utils import Schedule
@@ -12,4 +22,10 @@ class FCFS(Scheduler):
         super().__init__(process_list)
 
     def do_schedule(self) -> Schedule:
+        # sort and decide which process to be popped and use cpu
+        # sort process list by process.arrival_time
+        # for process in process_list:
+        # process.use_cpu(process.cpu_burst_time, self.current_time)
+        # self.current_time += process.cpu_burst_time
+        # self.schedule.add_schedule(process.process_ID, self.current_time)
         return self.schedule
