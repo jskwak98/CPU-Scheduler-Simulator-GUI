68d73dda2d5d670db165d9f3aff2776e7991e163
jskwak98 <jskwak98@naver.com>
2022-05-13 20:30:49 +0900

    Changed parameter names and order for the compliance with the requirements

--- process.py
+++ process.py
@@ -3,32 +3,32 @@ Define Process class
 
 Process class will be instantiated with following attributes
 
-process_name(str)       : name of the process; P1 P2 etc...; must be denoted at the Gantt Chart
-arrival_time(numeric)   : arrival time of the process; larger than 0
-cpu_burst_time(numeric) : required cpu burst time of process; larger than 0
-first_response(numeric) : required cpu burst time to give user the first response; le 0(?)
+process_ID(str)         : name of the process; P1 P2 etc...; must be denoted at the Gantt Chart
+arrival_time(int)       : arrival time of the process; larger than 0
+service_time(int)       : required cpu burst time to give user the first response; le 0(?)
 priority(int)           : priority of the process; le 0; smaller number means higher priority
+cpu_burst_time(int)     : required cpu burst time of process; larger than 0
 
 attributes above are stated in the document provided in class
 following attributes are made for convenience - based on personal opinion
 thus, can be modified at any time
 
-last_time_out(numeric)  : stores time when the process was context switched by the scheduler;
+last_time_out(int)      : stores time when the process was context switched by the scheduler;
                           can be used to compute WT, TT
-time_left(numeric)      : stores required cpu burst time to complete the process;
+time_left(int)          : stores required cpu burst time to complete the process;
                           initialized same as cpu_burst_time;
                           can be used to compute WT, TT, RT
-cumulative_wt(numeric)  : stores time the process was in the ready queue; WT itself
-response_time(numeric)  : actual response time
+cumulative_wt(int)      : stores time the process was in the ready queue; WT itself
+response_time(int)      : actual response time
 """
 
 class Process:
 
-    def __init__(self, process_name: str, arrival_time: int | float, cpu_burst_time: int | float, first_response: int | float, priority: int):
-        self.process_name = process_name
+    def __init__(self, process_ID: str, arrival_time: int, service_time: int, priority: int, cpu_burst_time: int):
+        self.process_ID = process_ID
         self.arrival_time = arrival_time
+        self.service_time = service_time
         self.cpu_burst_time = cpu_burst_time
-        self.first_response = first_response
         self.priority = priority
 
         self.last_time_out = -1
@@ -36,9 +36,9 @@ class Process:
         self.cumulative_wt = 0
         self.response_time = -1
 
-    def use_cpu(self, time_used: int | float, current_time: int | float) -> bool:
+    def use_cpu(self, time_used: int, current_time: int) -> bool:
         """
-        :param time_used: passed from scheduler, time using cpu, numeric
+        :param time_used: passed from scheduler, time using cpu, int
         :param current_time: passed from scheduler, current time
         :return(?) process_done: bool flag notifying if the process is complete or not
 
