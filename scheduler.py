47b40cd41eb4ad16bc720098dd6c57c4006c03af
jskwak98 <jskwak98@naver.com>
2022-05-14 01:09:57 +0900

    added current_time attribute to be inherited

--- scheduler.py
+++ scheduler.py
@@ -9,10 +9,13 @@ class Scheduler(metaclass=ABCMeta):
 
     common attributes are process_list, which is list of Process objects
     and schedule, which is a Schedule object
+
+    +) also added current_time attribute, to keep track of the time while scheduling
     """
     def __init__(self, process_list: list[Process]):
         self.process_list = process_list
         self.schedule = Schedule()
+        self.current_time = 0
 
     @abstractmethod
     def do_schedule(self) -> Schedule:
