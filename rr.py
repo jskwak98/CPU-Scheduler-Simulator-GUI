0ed97692f56941f26aef0da64b8f5124356934a7
Temirlan Tlektessov <58460135+ttlektessov@users.noreply.github.com>
2022-05-30 04:27:37 +0900

    Add files via upload

---
+++ rr.py
@@ -0,0 +1,64 @@
+import heapq
+from collections import deque
+
+from scheduler import Scheduler
+from utils import Schedule
+from process import Process
+
+
+class RR(Scheduler):
+
+    def __init__(self, process_list: list[Process], time_quantum: int):
+        """
+        attributes
+        self.pq : pq is a priority queue, saving (priority, last time the process came out from cpu, pid)
+                first two values are used to sort and decide which process to use
+        self.process_list : sorted ascending order by arrival_time for convenience
+        self.process_dict : it'll store process, key : pid, value : Process
+        """
+        super().__init__(process_list)
+        self.pq = []
+        self.time_quantum = time_quantum
+        self.process_list = deque(sorted(process_list, key=lambda x: x.arrival_time))
+        self.process_dict = {}
+        for p in process_list:
+            self.process_dict[p.process_ID] = p
+
+    def do_schedule(self) -> Schedule:
+        """
+        last_out_process is pointer variable which stores the last process used CPU
+        """
+        last_out_process = None
+
+        while True:
+            if self.process_list:
+                # put process that arrived since the last process used cpu until current time into ready queue(self.pq)
+                if self.process_list[0].arrival_time <= self.current_time:
+                #현재시간 증가함에 따라, 프로세스 도착하면 process_list에서 빼다가 pq에 넣어주기  
+                    while self.process_list and self.process_list[0].arrival_time <= self.current_time:
+                        p = self.process_list.popleft()
+                        heapq.heappush(self.pq, (p.last_time_out, p.process_ID))
+                # No process to use cpu in current time, proceed time to the arrival time of the next process
+                if not last_out_process and not self.pq: #지금 당장 실행할 프로세스가 없는 경우: empty
+                    self.current_time = self.process_list[0].arrival_time
+                    self.schedule.add_schedule("None", self.current_time)
+                    continue
+            # all process is complete
+            elif not last_out_process and not self.pq:
+                break
+            # put the incomplete back to the ready queue
+            if last_out_process: 
+                # slight tie breaker needed, add small number to make sure it's behind the process who went in right now
+                heapq.heappush(self.pq, (last_out_process.last_time_out + 0.001, last_out_process.process_ID))
+
+            # pop from the pq, that's the process that should use cpu right now
+            pid = heapq.heappop(self.pq)[1]
+            last_out_process = self.process_dict[pid]
+            use_time = self.time_quantum if self.time_quantum <= last_out_process.time_left else last_out_process.time_left
+            last_out_process.use_cpu(use_time, self.current_time)
+            self.current_time += use_time
+            self.schedule.add_schedule(last_out_process.process_ID, self.current_time)
+            if last_out_process.process_complete:
+                last_out_process = None
+
+        return self.schedule
--- srtf.py
+++ srtf.py
@@ -1,88 +1,43 @@
+from collections import deque
 from scheduler import Scheduler
 from utils import Schedule
 from process import Process
+import heapq
 
 
-class srtf():
+class SRTF(Scheduler):
+
     def __init__(self, process_list: list[Process]):
-        self.process_list = process_list
+        super().__init__(process_list)
+        self.pq = []
+        self.process_list = deque(sorted(process_list, key=lambda x: x.arrival_time))
+        self.process_dict = {}
+        for p in process_list:
+            self.process_dict[p.process_ID] = p
 
-    def do_schedule(self, process_list):
-        start_time = []
-        exit_time = []
-        s_time = 0
-        gant = []
-        process_list.sort(key=lambda x: x[1])
-        '''
-        Sort processes according to the Arrival Time
-        '''
-        while 1:
-            ready_queue = []
-            normal_queue = []
-            temp = []
-            for i in range(len(process_list)):
-                if process_list[i][1] <= s_time and process_list[i][3] == 0:
-                    temp.extend([process_list[i][0], process_list[i][1], process_list[i][2], process_list[i][4]])
-                    ready_queue.append(temp)
-                    temp = []
-                elif process_list[i][3] == 0:
-                    temp.extend([process_list[i][0], process_list[i][1], process_list[i][2], process_list[i][4]])
-                    normal_queue.append(temp)
-                    temp = []
-            if len(ready_queue) == 0 and len(normal_queue) == 0:
+    def do_schedule(self) -> Schedule:
+        # pointer variable
+        last_out_process = None
+        while True:
+            if self.process_list:
+                while self.process_list and self.process_list[0].arrival_time <= self.current_time:
+                    p = self.process_list.popleft()
+                    heapq.heappush(self.pq, (p.cpu_burst_time, p.process_ID))
+                if not last_out_process and not self.pq:
+                    self.current_time = self.process_list[0].arrival_time
+                    self.schedule.add_schedule("None", self.current_time)
+                    continue
+            elif not last_out_process and not self.pq:
                 break
-            if len(ready_queue) != 0:
-                ready_queue.sort(key=lambda x: x[2])
-                '''
-                Sort processes according to Burst Time
-                '''
-                start_time.append(s_time)
-                s_time = s_time + 1
-                e_time = s_time
-                exit_time.append(e_time)
-                gant.append(ready_queue[0][0])
-                for k in range(len(process_list)):
-                    if process_list[k][0] == ready_queue[0][0]:
-                        break
-                process_list[k][2] = process_list[k][2] - 1
-                if process_list[k][2] == 0:  # If Burst Time of a process is 0, it means the process is completed
-                    process_list[k][3] = 1
-                    process_list[k].append(e_time)
-            if len(ready_queue) == 0:
-                if s_time < normal_queue[0][1]:
-                    s_time = normal_queue[0][1]
-                start_time.append(s_time)
-                s_time = s_time + 1
-                e_time = s_time
-                exit_time.append(e_time)
-                gant.append(normal_queue[0][0])
-                for k in range(len(process_list)):
-                    if process_list[k][0] == normal_queue[0][0]:
-                        break
-                process_list[k][2] = process_list[k][2] - 1
-                if process_list[k][2] == 0:  # If Burst Time of a process is 0, it means the process is completed
-                    process_list[k][3] = 1
-                    process_list[k].append(e_time)
+            if last_out_process:
+                heapq.heappush(self.pq, (last_out_process.cpu_burst_time, last_out_process.process_ID))
 
-        total_turnaround_time = 0
-        for i in range(len(process_list)):
-            turnaround_time = process_list[i][5] - process_list[i][1]
-            '''
-            turnaround_time = completion_time - arrival_time
-            '''
-            total_turnaround_time = total_turnaround_time + turnaround_time
-            process_list[i].append(turnaround_time)
-        average_turn = total_turnaround_time / len(process_list)
+            pid = heapq.heappop(self.pq)[1]
+            last_out_process = self.process_dict[pid]
+            last_out_process.use_cpu(1, self.current_time)
+            self.current_time += 1
+            self.schedule.add_schedule(pid, self.current_time)
+            if last_out_process.process_complete:
+                last_out_process = None
 
-        total_waiting_time = 0
-        for i in range(len(process_list)):
-            waiting_time = process_list[i][6] - process_list[i][4]
-            '''
-            waiting_time = turnaround_time - burst_time
-            '''
-            total_waiting_time = total_waiting_time + waiting_time
-            process_list[i].append(waiting_time)
-        average_wait = total_waiting_time / len(process_list)
-
-
-        return self.process_list, average_wait, average_turn, gant
+        return self.schedule
