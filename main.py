eec38ffadb22bdcf3e99d2a919af05885f518726
jskwak98 <jskwak98@naver.com>
2022-05-30 23:18:58 +0900

    Built .exe file

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from GUI import SimulatorGUI
 import sys
 from PyQt5.QtWidgets import QApplication
 
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    ex = SimulatorGUI()
-    sys.exit(app.exec_())
+
+app = QApplication(sys.argv)
+ex = SimulatorGUI()
+sys.exit(app.exec_())
